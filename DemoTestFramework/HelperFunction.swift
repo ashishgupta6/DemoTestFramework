2d5516398e8e220af4855a158d855828af1ae32e
Ashish Gupta <ashish.gupta@sign3labs.com>
2024-12-05 22:30:19 +0530

    forst commit

---
+++ DemoTestFramework/HelperFunction.swift
@@ -0,0 +1,25 @@
+//
+//  HelperFunction.swift
+//  DemoFramework
+//
+//  Created by Ashish Gupta on 04/12/24.
+//
+
+import Foundation
+import UIKit
+
+public struct HelperFunction {
+    
+    public static func showLogs(key: String, value: String){
+        print("\(key): \(value)")
+    }
+    
+    public static func getApplicationId() -> String {
+        return Bundle.main.bundleIdentifier ?? "Unknown"
+    }
+    
+    @MainActor
+    public static func getDeviceModel() -> String {
+        return UIDevice.current.model
+    }
+}
