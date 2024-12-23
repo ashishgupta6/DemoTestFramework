ff09e0056c4a4d6d1574d528f959f7088cc4ce88
Ashish Gupta <ashish.gupta@sign3labs.com>
2024-12-23 13:10:34 +0530

    m,inrochanges

---
+++ DemoTestFramework/Private/PrintName.swift
@@ -0,0 +1,16 @@
+//
+//  HelperFunction.swift
+//  DemoFramework
+//
+//  Created by Ashish Gupta on 04/12/24.
+//
+
+import UIKit
+
+internal struct PrintName {
+    
+    internal static func printName(key: String, value: String){
+        print("Name: \(value)")
+    }
+
+}
---
+++ DemoTestFramework/Public/DemoTest.swift
@@ -0,0 +1,23 @@
+//
+//  HelperFunction 2.swift
+//  DemoTestFramework
+//
+//  Created by Ashish Gupta on 23/12/24.
+//
+
+
+//
+//  HelperFunction.swift
+//  DemoFramework
+//
+//  Created by Ashish Gupta on 04/12/24.
+//
+
+import UIKit
+
+public struct DemoTest {
+    
+    public static func printYourName(key: String, value: String){
+        PrintName.printName(key: key, value: value)
+    }
+}
